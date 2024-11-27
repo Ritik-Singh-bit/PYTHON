9227e721f435eae0b1fb8ea45c7b3c2052432552
Ritik Singh <ritik36366@gmail.com>
2024-11-27 13:06:05 +0530

    SIMPLE CALCULATOR
    this is a simple calculator .you can run arithmetical operation

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=int(input("enter a num  : "))
+b=int(input("enter a num  : "))
+print(" for additon press A  , for subtraction press B , for multiplication press C ,  for divide press D , for All operations Press O ")
+c=input("code: ")
+match c:
+    case "A":
+        print(a+b)
+    case "B":
+         print(a-b)
+         print(a*b)    
+    case "D" :
+        print(a/b)
+    case "O" :
+        print(a+b,a-b,a*b,a/b)
+    case default :
+        print("wrong input")
